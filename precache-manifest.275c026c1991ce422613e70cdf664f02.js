7b09979fe398f6f94d75d6e898eda94859462f3b
Arpit Sheth <shetharp@gmail.com>
2019-02-04 18:28:49 -0500

    Updates

---
+++ precache-manifest.275c026c1991ce422613e70cdf664f02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "67a493c7d2aa84e9684c1f2edffea597",
+    "url": "/card-game/static/media/game-bg.67a493c7.svg"
+  },
+  {
+    "revision": "7dcc97aa1e4f751369038067f54db60c",
+    "url": "/card-game/static/media/card-bg.7dcc97aa.svg"
+  },
+  {
+    "revision": "06015fac58d7e9563f71",
+    "url": "/card-game/static/js/runtime~main.06015fac.js"
+  },
+  {
+    "revision": "4610b9e4a99ce3ef7be7",
+    "url": "/card-game/static/js/main.4610b9e4.chunk.js"
+  },
+  {
+    "revision": "f929043208f3d998fba8",
+    "url": "/card-game/static/js/1.f9290432.chunk.js"
+  },
+  {
+    "revision": "bebf9203ebcdce7cbf442e351108eaa7",
+    "url": "/card-game/index.html"
+  }
+];
