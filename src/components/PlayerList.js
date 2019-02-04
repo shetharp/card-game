32a3281a181f1579b60b2e77eedd9f48754055fd
Arpit Sheth <shetharp@gmail.com>
2019-02-04 13:26:02 -0500

    Ensures PlayerList is sorted by player name

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,11 +1,28 @@
 import React from "react";
 import Hand from "./Hand";
 
+// ==================================================
+//  HELPER FUNCTIONS
+// ==================================================
+/**
+ * Returns a sorted COPY of playerState objects, sorted by the 'name' property 
+ * @param {playerState[]} playerStates A list of player state objects that should be sorted by name
+ */
+function getSortedPlayerStates(playerStates) {
+  const sortedPlayerStates = playerStates.slice();
+  sortedPlayerStates.sort((p1Obj, p2Obj) => {
+    if (p1Obj.name < p2Obj.name) { return -1; }
+    if (p1Obj.name > p2Obj.name) { return 1; }
+    return 0;
+  });
+  return sortedPlayerStates;
+}
+
 function PlayerList(props) {
-  const players = props.players.map(({ name, hand, numCards }) => {
+  const sortedPlayerStates = getSortedPlayerStates(props.players);
+  const players = sortedPlayerStates.map(({ name, hand, numCards }) => {
     return (
       <li key={name}>
-        {/* <Hand playerName={name} cards={hand} /> */}
         <Hand playerName={name} cards={hand} />
       </li>
     );
